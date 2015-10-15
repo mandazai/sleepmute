2e2f7e84017e8754b16e222f4db2f0b77dd74355
scola <shaozheng.wu@gmail.com>
2015-10-15 10:56:46 +0800

    first commit

---
+++ app/src/androidTest/java/sample/datetimepicker/fourmob/com/datetimesample/ApplicationTest.java
@@ -0,0 +1,13 @@
+package sample.datetimepicker.fourmob.com.datetimesample;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
