109156ba1bc750a73921ce387d46fcdd0f657179
scola <shaozheng.wu@gmail.com>
2015-10-19 16:38:52 +0800

    add switch menu

--- app/src/main/java/sample/datetimepicker/fourmob/com/datetimesample/MainActivity.java
+++ app/src/main/java/sample/datetimepicker/fourmob/com/datetimesample/MainActivity.java
@@ -3,6 +3,8 @@ package sample.datetimepicker.fourmob.com.datetimesample;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.CheckBox;
@@ -27,5 +29,27 @@ public class MainActivity extends FragmentActivity {
         setContentView(R.layout.radial_time_circle);
 
         mTimeView = (RadialTimeView) findViewById(R.id.time_circle);
+    }
+    
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.switch_airplane) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
